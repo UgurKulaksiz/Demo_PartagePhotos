bcd34d4ad4c2b9f116863ceb9c171719c664d0b1
UgurKulaksiz <ugur.kulaksiz@outlook.com>
2026-05-08 16:05:46 +0200

    Ajout du premier composant FaceSnap

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,14 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { FaceSnap } from './face-snap/face-snap';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [
+    FaceSnap // Le composant FaceSnap est importé pour être utilisé dans le template de ce composant
+  ],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
-  protected readonly title = signal('Demo_PartagePhotos');
 }
---
+++ src/app/face-snap/face-snap.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-face-snap', // Le nom de la balise HTML qui représentera ce composant
+  standalone: true,
+  imports: [],
+  templateUrl: './face-snap.html',
+  styleUrl: './face-snap.scss',
+})
+export class FaceSnap {}
