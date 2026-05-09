c98ca92b53359063ef33cdd058d470b8f6cb5f50
UgurKulaksiz <ugur.kulaksiz@outlook.com>
2026-05-09 14:04:56 +0200

    Afficher des données.

--- src/app/face-snap/face-snap.ts
+++ src/app/face-snap/face-snap.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-face-snap', // Le nom de la balise HTML qui représentera ce composant
@@ -7,4 +7,19 @@ import { Component } from '@angular/core';
   templateUrl: './face-snap.html',
   styleUrl: './face-snap.scss',
 })
-export class FaceSnap {}
+export class FaceSnap implements OnInit {
+  title!: string;
+  description!: string;
+  createdAt!: Date;
+  snaps!: number;
+  imageUrl!: string;
+
+  // Méthode appelée lors de l'initialisation du composant
+  ngOnInit(): void {
+    this.title = "Archibald";
+    this.description = "Mon meilleur ami depuis toujours !";
+    this.createdAt = new Date();
+    this.snaps = 5;
+    this.imageUrl = "https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-792273_1280.jpg";
+  }
+}
