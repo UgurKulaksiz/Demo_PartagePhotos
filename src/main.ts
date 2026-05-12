8f411b85f3f661a05a29e143ad73fc6191a3f098
UgurKulaksiz <ugur.kulaksiz@outlook.com>
2026-05-12 14:25:16 +0200

    Formater les dates

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,10 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { App } from './app/app';
+import { registerLocaleData } from '@angular/common';
+import * as fr from '@angular/common/locales/fr';
+
+registerLocaleData(fr.default); // Enregistrement de la locale française pour les pipes de date et de nombre
 
 bootstrapApplication(App, appConfig)
   .catch((err) => console.error(err));
