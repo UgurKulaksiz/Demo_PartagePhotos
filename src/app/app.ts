55a6dc2cc4b29f7b8106adc6cac1c1a12b3eb67b
UgurKulaksiz <ugur.kulaksiz@outlook.com>
2026-05-12 12:23:39 +0200

    Afficher des listes

--- src/app/app.ts
+++ src/app/app.ts
@@ -12,35 +12,37 @@ import { FaceSnapModel } from './models/face-snap-model';
   styleUrl: './app.scss'
 })
 export class App implements OnInit {
+  faceSnaps!: FaceSnapModel[];
+
   mySnap!: FaceSnapModel;
   myOtherSnap!: FaceSnapModel;
   myLastSnap!: FaceSnapModel;
 
   ngOnInit(): void {
-    this.mySnap = new FaceSnapModel(
-      "Archibald",
-      "Mon meilleur ami depuis toujours !",
-      "https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-792273_1280.jpg",
-      new Date(),
-      10
-    );
+    this.faceSnaps = [
+      new FaceSnapModel(
+        "Archibald",
+        "Mon meilleur ami depuis toujours !",
+        "https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-792273_1280.jpg",
+        new Date(),
+        10
+      ),
+      // new FaceSnapModel(
+      //   "Three Rock Mountain",
+      //   "Un endroit magnifique pour les randonnées !",
+      //   "https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Three_Rock_Mountain_Southern_Tor.jpg/960px-Three_Rock_Mountain_Southern_Tor.jpg",
+      //   new Date(),
+      //   6
+      // ),
+      // new FaceSnapModel(
+      //   "Un bon repas",
+      //   "Mmmh que c'était bon !",
+      //   "https://wtop.com/wp-content/uploads/2020/06/HEALTHYFRESH.jpg",
+      //   new Date(),
+      //   156
+      // )
+    ];
 
-    this.myOtherSnap = new FaceSnapModel(
-      "Three Rock Mountain",
-      "Un endroit magnifique pour les randonnées !",
-      "https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Three_Rock_Mountain_Southern_Tor.jpg/960px-Three_Rock_Mountain_Southern_Tor.jpg",
-      new Date(),
-      6
-    );
-    
-    this.myLastSnap = new FaceSnapModel(
-      "Un bon repas",
-      "Mmmh que c'était bon !",
-      "https://wtop.com/wp-content/uploads/2020/06/HEALTHYFRESH.jpg",
-      new Date(),
-      156
-    );
-
-    this.myOtherSnap.setLocation("à la montagne");
+    this.faceSnaps[0].setLocation("à la montagne");
   }
 }
